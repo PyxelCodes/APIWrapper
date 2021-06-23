b86e73499648d4d4f891ec220cc928687be2b54b
Pyxel <pyxeldev@icloud.com>
2021-06-23 13:06:09 +0200

    oops v2

--- next/next.config.js
+++ next/next.config.js
@@ -1,36 +1,36 @@
 /* eslint-disable global-require */
 module.exports = {
-    distDir: 'build',
-    darkMode: "class",
-    future: {
-      webpack5: true,
+  distDir: 'build',
+  darkMode: "class",
+  future: {
+    webpack5: true,
+  },
+  purge: {
+    content: ["./src/**/*.tsx", "./public/index.html"],
+    options: {
+      safelist: ["h-8", "h-11"],
     },
-    purge: {
-      content: ["./src/**/*.tsx", "./public/index.html"],
-      options: {
-        safelist: ["h-8", "h-11"],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/intro',
+        permanent: true,
       },
-    },
-    async redirects() {
-      return [
-        {
-          source: '/docs',
-          destination: '/docs/intro',
-          permanent: true,
-        },
-        {
-          source: '/favicon.ico',
-          destination: 'https://reefraid.com/favicon.ico',
-          permanent: true,
-        }
-      ]
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/api/v1/:path*',
-          destination: process.env.NODE_ENV === 'development' ? 'http://localhost/v1/:path*' : 'https://stats.reefraid.com/api/v1/:path*'
-        }
-      ]
-    }
-  };
+      {
+        source: '/favicon.ico',
+        destination: 'https://reefraid.com/favicon.ico',
+        permanent: true,
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: process.env.NODE_ENV === 'development' ? 'http://localhost/v1/:path*' : 'https://ws.hangle.me/v1/:path*'
+      }
+    ]
+  }
+};
